090ad3347284e2765003cca334227babc34193aa
kento.uoi <fishperson19@gmail.com>
2021-02-13 01:57:28 +0900

    keyword-search

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 require('dotenv').config()
-const axios = require('axios')
-const querystring = require('querystring')
-
-const baseUrl = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
-const keyword = 'yakiniku'
-const params = {
-  key: process.env.API_KEY,
-  format: 'json',
-  keyword: `${keyword}`,
-  count: 5,
-}
-const result = querystring.stringify(params)
-console.log(result)
-const url = `${baseUrl}?${result}`
-
-const getRequest = axios.get(url)
-const getAll = getRequest
-  .then(response => {
-    if (response) {
-      return response.data
-    } else {
-      response.status(404)
-    }
-  })
-  .catch(error => {
-    console.log(error)
-  })
-
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const axios = require('axios')
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+const querystring = require('querystring')
+const baseUrl = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+
+const getShop = (keyword) => {
+  const params = {
+    key: process.env.API_KEY,
+    format: 'json',
+    keyword: `${keyword}`,
+    count: 5,
+  }
+  const result = querystring.stringify(params)
+  // console.log(`渡すクエリは${result}`)
+  const apiUrl = `${baseUrl}?${result}`
+  console.log(apiUrl)
+  return (
+    axios
+      .get(apiUrl)
+      .then(response => {
+        if (response) {
+          return response.data["results"]["shop"]
+        } else {
+          response.status(404)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  )
+}
 
 app.get('/', (request, response) => {
-  getAll.then(shops => { 
-    response.json(shops)
+  const body = request.body
+  console.log(body)
+  getShop(body)
+    .then(shops => {
+      response.json(shops)
+      console.log(shops)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+// このPOSTはフロントにデータを取りに行くためPOST
+
+app.post('/', (request, response, next) => {
+  const body = request.body.keyword
+  console.log(body)
+  getShop(body)
+    .then(shops => {
+      response.json(shops)
   })
 })
-
 const PORT = 3001
 app.listen(PORT, () => {
   console.log('running')
