4efffa4b8f08af475aa46f60dfc32baf740ced66
kento.uoi <fishperson19@gmail.com>
2021-03-18 18:53:51 +0900

    build

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const shopsRouter = require('./controllers/shops')
 const genresRouter = require('./controllers/genres')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use('/api/shops', shopsRouter)
 app.use('/api/genres', genresRouter)
