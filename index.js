430bdababea96df2a662d1f38852808eac8c1d9f
kento.uoi <fishperson19@gmail.com>
2021-03-18 17:58:34 +0900

    Procfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const http = require('http')
 
 const server = http.createServer(app)
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 server.listen(PORT, () => {
   console.log('running')
