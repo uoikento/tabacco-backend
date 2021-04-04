d9bf6c3a30b1a8c1cee0787f1563663169cc02a4
kento.uoi <fishperson19@gmail.com>
2021-04-04 14:05:42 +0900

    correctBuild

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const genresRouter = require('./controllers/genres')
 const areasRouter = require('./controllers/areas')
 
 app.use(cors())
-app.use(express.static('build'))
+// app.use(express.static('build'))
 app.use(express.json())
 app.use('/api/shops', shopsRouter)
 app.use('/api/genres', genresRouter)
